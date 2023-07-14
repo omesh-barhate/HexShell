775c036d444ed271c55eddd0a8d43aa4df166cb3
Omesh B <barhateomesh@gmail.com>
2023-07-14 06:10:59 +0530

    Background processes added

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# Basic Shell Implementation
+# HexShell
 
-This is a simple implementation of a shell program in C. The shell provides a command-line interface for executing other programs and basic built-in commands.
+This is a simple implementation of a shell program in C. The shell provides a command-line interface for executing other programs and basic built-in commands. It has been updated to include additional features and improve the user experience.
 
 ## Getting Started
 
 To compile the program, run the following command in the terminal:
 
-gcc shell.c -o shell
+gcc shell.c -o shell -lncurses
 
 
 To run the shell, simply execute the compiled binary:
@@ -14,26 +14,70 @@ To run the shell, simply execute the compiled binary:
 ./shell
 
 
-The shell will display a prompt (`>>`) and wait for user input. The user can enter commands, which will be executed by the shell. The shell also provides two built-in commands: `cd` and `exit`.
+The shell will display a prompt (`>>`) and wait for user input. The user can enter commands, which will be executed by the shell. The shell provides built-in commands such as `cd` for changing the current working directory and `exit` for exiting the shell.
 
 ## Features
 
 The basic shell implementation provides the following features:
 
-- A simple command-line interface for executing other programs
-- Support for parsing user input into separate arguments
-- Built-in commands for changing the current working directory (`cd`) and exiting the shell (`exit`)
-- Forking and executing commands in a child process
-- Waiting for child processes to complete using the `waitpid` function
-- Handling signals such as `SIGINT` (Ctrl-C) and `SIGTSTP` (Ctrl-Z)
+- command-line interface with prompt (>>) for user input
+- Parsing user input into separate commands and arguments
+- Execution of single commands or command pipelines
+- Input/output redirection and piping
+- Handling of signals such as SIGINT (Ctrl-C) and SIGTSTP (Ctrl-Z)
+- Support for background process execution using the & symbol
+- Basic error handling and reporting
+
+## Usage
+
+The shell supports the following features:
+
+### Built-in Commands
+
+The shell provides two built-in commands:
+
+- cd [directory]: Change the current working directory to the specified directory. If no directory is provided, it changes to the user's home directory.
+- exit: Exit the shell.
+
+### Command Execution
+
+The shell can execute external commands by entering their name followed by any required arguments. For example:
+
+>> ls -l
+
+### Command Pipelines
+The shell supports executing command pipelines using the `|` symbol. For example:
+
+>> ls -l | grep shell
+
+This command lists files and directories in the current directory and pipes the output to `grep` to search for the word "shell".
+
+### Input/Output Redirection
+
+The shell supports input/output redirection using the `<`, `>`, and `>>` symbols. For example:
+
+>> ls > files.txt
+
+This command lists files and directories in the current directory and redirects the output to a file named "files.txt".
+
+### Background Processes
+
+To execute a command in the background, append an `&` symbol at the end of the command. For example:
+
+>> sleep 10 &
+
+This command executes the `sleep` command in the background, causing the shell to return immediately.
+
 
 ## Limitations
 
-The basic shell implementation has several limitations, including:
+The basic shell implementation has some limitations, including:
 
-- Lack of support for advanced features such as input/output redirection, piping, and background processes
-- No support for command history or auto-completion
-- Limited error handling and error reporting
+- Lack of advanced features such as command history, auto-completion, and environment variable handling
+- Limited error handling and reporting
+- Not suitable for complex shell scripting
+
+The shell is intended as a basic starting point and can be extended to include additional features based on specific requirements.
 
 
 
--- shell.c
+++ shell.c
@@ -2,78 +2,298 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <signal.h>
+#include <ncurses.h>
 
 #define MAX_INPUT_LENGTH 1024
 #define MAX_ARGS 64
+#define MAX_BACKGROUND_PROCESSES 64
+
+pid_t background_processes[MAX_BACKGROUND_PROCESSES];
+int num_background_processes = 0;
+
+int redirect_input(const char* filename) {
+    // Open the file for reading and redirect standard input to it
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("fopen");
+        return -1;
+    }
+    if (dup2(fileno(file), STDIN_FILENO) == -1) {
+        perror("dup2");
+        fclose(file);
+        return -1;
+    }
+    fclose(file);
+    return 0;
+}
+
+int redirect_output(const char* filename) {
+    // Open the file for writing and redirect standard output to it
+    FILE* file = fopen(filename, "w");
+    if (file == NULL) {
+        perror("fopen");
+        return -1;
+    }
+    if (dup2(fileno(file), STDOUT_FILENO) == -1) {
+        perror("dup2");
+        fclose(file);
+        return -1;
+    }
+    fclose(file);
+    return 0;
+}
+
+int append_output(const char* filename) {
+    // Open the file for appending and redirect standard output to it
+    FILE* file = fopen(filename, "a");
+    if (file == NULL) {
+        perror("fopen");
+        return -1;
+    }
+    if (dup2(fileno(file), STDOUT_FILENO) == -1) {
+        perror("dup2");
+        fclose(file);
+        return -1;
+    }
+    fclose(file);
+    return 0;
+}
+
+void restore_io() {
+    // Restore standard input and output to their default values
+    dup2(STDIN_FILENO, fileno(stdin));
+    dup2(STDOUT_FILENO, fileno(stdout));
+}
 
 void handle_signal(int sig) {
     if (sig == SIGINT) {
-        printf("Received SIGINT signal, terminating the current process.\n");
+        printw("\nReceived SIGINT signal, terminating the current process.\n");
+        refresh();
         exit(EXIT_FAILURE);
     } else if (sig == SIGTSTP) {
-        printf("Received SIGTSTP signal, stopping the current process.\n");
-        //TODO: Implement process suspension logic here
+        printw("\nReceived SIGTSTP signal, stopping the current process.\n");
+        refresh();
+        // TODO: Implement process suspension logic here
     }
 }
 
+int execute_pipeline(char* commands[MAX_ARGS][MAX_ARGS], int num_commands) {
+    int pipes[num_commands - 1][2];
+    pid_t pids[num_commands];
+    int i;
+
+    // Create pipes
+    for (i = 0; i < num_commands - 1; i++) {
+        if (pipe(pipes[i]) == -1) {
+            perror("pipe");
+            return -1;
+        }
+    }
+
+    // Fork and execute commands
+    for (i = 0; i < num_commands; i++) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            // Child process
+
+            // Set up input redirection for the first command
+            if (i == 0) {
+                if (commands[i][1] != NULL) {
+                    if (redirect_input(commands[i][1]) == -1) {
+                        exit(EXIT_FAILURE);
+                    }
+                }
+            } else {
+                // Set up input redirection from the previous pipe
+                if (dup2(pipes[i - 1][0], STDIN_FILENO) == -1) {
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+                close(pipes[i - 1][0]);
+            }
+
+            // Set up output redirection for the last command
+            if (i == num_commands - 1) {
+                if (commands[i][2] != NULL) {
+                    if (redirect_output(commands[i][2]) == -1) {
+                        exit(EXIT_FAILURE);
+                    }
+                }
+            } else {
+                // Set up output redirection to the current pipe
+                if (dup2(pipes[i][1], STDOUT_FILENO) == -1) {
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+                close(pipes[i][1]);
+            }
+
+            // Close all pipes
+            for (int j = 0; j < num_commands - 1; j++) {
+                close(pipes[j][0]);
+                close(pipes[j][1]);
+            }
+
+            if (execvp(commands[i][0], commands[i]) == -1) {
+                perror("execvp");
+                exit(EXIT_FAILURE);
+            }
+        } else if (pid < 0) {
+            // Error forking
+            perror("fork");
+            return -1;
+        } else {
+            // Parent process
+            pids[i] = pid;
+        }
+    }
+
+    // Close all pipes in the parent process
+    for (i = 0; i < num_commands - 1; i++) {
+        close(pipes[i][0]);
+        close(pipes[i][1]);
+    }
+
+    // Wait for all child processes to finish
+    int status;
+    for (i = 0; i < num_commands; i++) {
+        waitpid(pids[i], &status, 0);
+        if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
 int main() {
-  char input[MAX_INPUT_LENGTH];
-  char *args[MAX_ARGS];
-  pid_t pid;
-  int status;
-  signal(SIGINT, handle_signal);
-  signal(SIGTSTP, handle_signal);
-    
-  // Shell loop
-  while (1) {
-    // Prompt user for input
-    printf(">> ");
-    fflush(stdout);
+    char input[MAX_INPUT_LENGTH];
+    char *commands[MAX_ARGS][MAX_ARGS];
+    int num_commands;
+    pid_t pid;
+    int status;
 
-    // Read user input
-    fgets(input, MAX_INPUT_LENGTH, stdin);
+    // Initialize ncurses
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
 
-    // Parse user input into separate arguments
-    char *token = strtok(input, " \t\n");
-    int i = 0;
-    while (token != NULL && i < MAX_ARGS - 1) {
-      args[i++] = token;
-      token = strtok(NULL, " \t\n");
+    signal(SIGINT, handle_signal);
+    signal(SIGTSTP, handle_signal);
+
+    // Shell loop
+    while (1) {
+        // Prompt user for input
+        printw(">> ");
+        refresh();
+
+        // Read user input
+        getstr(input);
+	input[strcspn(input, "\n")] = '\0';
+
+        // Parse user input into separate commands
+        char *token = strtok(input, "|\n");
+        num_commands = 0;
+        while (token != NULL && num_commands < MAX_ARGS) {
+            char *arg_token = strtok(token, " \t");
+            int i = 0;
+            while (arg_token != NULL && i < MAX_ARGS - 1) {
+                commands[num_commands][i++] = arg_token;
+                arg_token = strtok(NULL, " \t");
+            }
+            commands[num_commands][i] = NULL;
+
+            num_commands++;
+            token = strtok(NULL, "|\n");
+        }
+
+        // Check for built-in commands
+        if (strcmp(commands[0][0], "cd") == 0) {
+            // Handle cd command
+            if (chdir(commands[0][1]) != 0) {
+                perror("cd");
+            }
+            continue;
+        } else if (strcmp(commands[0][0], "exit") == 0) {
+            // Handle exit command
+            break;
+        }
+
+        // Fork process and execute commands
+        pid = fork();
+        if (pid == 0) {
+            // Child process
+
+            // Set up input redirection for the first command
+            if (num_commands > 1) {
+                if (redirect_input(commands[0][1]) == -1) {
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            // Set up output redirection for the last command
+            if (num_commands > 1) {
+                if (redirect_output(commands[num_commands - 1][2]) == -1) {
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            if (num_commands == 1) {
+                // Execute a single command
+                if (execvp(commands[0][0], commands[0]) == -1) {
+                    perror("execvp");
+                    exit(EXIT_FAILURE);
+                }
+            } else {
+                // Execute a pipeline of commands
+                if (execute_pipeline(commands, num_commands) == -1) {
+                    exit(EXIT_FAILURE);
+                }
+            }
+        } else if (pid < 0) {
+            // Error forking
+            perror("fork");
+        } else {
+            // Parent process
+            if (commands[num_commands - 1][0][0] != '&') {
+                // Wait for foreground process to finish
+                do {
+                    waitpid(pid, &status, WUNTRACED);
+                } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+            } else {
+                // Background process, add PID to the array
+                background_processes[num_background_processes++] = pid;
+            }
+
+            // Check for completed background processes
+            int i = 0;
+            while (i < num_background_processes) {
+                pid_t bg_pid = background_processes[i];
+                int result = waitpid(bg_pid, &status, WNOHANG);
+                if (result == -1) {
+                    // Error
+                    perror("waitpid");
+                } else if (result > 0) {
+                    // Process completed, remove PID from the array
+                    int j;
+                    for (j = i; j < num_background_processes - 1; j++) {
+                        background_processes[j] = background_processes[j + 1];
+                    }
+                    num_background_processes--;
+                } else {
+                    i++;
+                }
+            }
+        }
     }
-    args[i] = NULL;
 
-    // Check for built-in commands
-    if (strcmp(args[0], "cd") == 0) {
-      // Handle cd command
-      if (chdir(args[1]) != 0) {
-        perror("cd");
-      }
-      continue;
-    } else if (strcmp(args[0], "exit") == 0) {
-      // Handle exit command
-      exit(0);
-    }
+    // Clean up ncurses
+    endwin();
 
-    // Fork process and execute command
-    pid = fork();
-    if (pid == 0) {
-      // Child process
-      if (execvp(args[0], args) == -1) {
-        perror("execvp");
-        exit(EXIT_FAILURE);
-      }
-    } else if (pid < 0) {
-      // Error forking
-      perror("fork");
-    } else {
-      // Parent process
-      do {
-        waitpid(pid, &status, WUNTRACED);
-      } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
-  }
-
-  return 0;
+    return 0;
 }
 
